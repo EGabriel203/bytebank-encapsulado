749d340ea2fd46fd51bc3cbf92d7c90d6ed7faa5
EGabriel203 <104772266+EGabriel203@users.noreply.github.com>
2022-10-14 10:23:50 -0300

    Add files via upload

---
+++ Conta.java
@@ -0,0 +1,82 @@
+class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total;
+
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		System.out.println("o total de contas e: " + total);
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("estou criando uma conta " + this.numero);
+	}
+
+	public static int getTotal() {
+		return Conta.total;
+	}
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(int novoNumero) {
+		if (novoNumero <= 0) {
+			System.out.println("nao pode numero negativo");
+			return;
+		}
+		this.numero = novoNumero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("não pode vlao menor ou igual a 0");
+			return;
+		}
+		this.agencia = agencia;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	void deposita(double valor) {
+		saldo = saldo + valor;
+
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+
+		}
+
+	}
+
+}
